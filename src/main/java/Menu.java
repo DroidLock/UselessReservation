f02b614e62b79fb4098137a5fe5d780427a35d12
DroidLock <DroidLock@users.noreply.github.com>
2019-02-22 16:03:08 +0100

    menue added

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,44 @@
+import java.sql.SQLOutput;
+
 public class Menu {
+
+    int auswahl;
+    ReservationService service;
+
+    public Menu(){
+
+    }
+
+    public void showMenu(){
+        System.out.println("**********************");
+        System.out.println("Willkommen bei Useless-Reservator");
+        System.out.println();
+        System.out.println("1. Reservierung erstellen");
+        System.out.println("2. Reservierungen anzeigen");
+        switch (auswahl){
+            case 1:
+        }
+    }
+
+    public void makeReservation(){
+        System.out.println();
+        System.out.println("***************************");
+        System.out.println();
+        System.out.println("Geben sie den Namen ein");
+
+        System.out.println("Geben sie die Anzahl der Personen ein");
+
+        System.out.println("Geben sie die Tischnummer ein");
+
+        System.out.println("Geben sie das Reservierungsdatum ein");
+
+        System.out.println("Geben sie die Reservierungszeit ein");
+
+
+    }
+
+    public void showReservations(){
+        service.getReservation();
+    }
+
 }
