92f1ddcfb5a92b19f385be0a647eb38da4378cec
Philipp Thaler <thalerphilipp1@gmail.com>
2019-02-23 12:20:52 +0100

    Fixed some edge cases

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -13,44 +13,50 @@ public class Parser {
     public String getString(String tag) {
         String input = scanner.nextLine();
 
-        if (tag.equals("name")) {
+        if (tag.equalsIgnoreCase("name")) {
             while (!validator.validateName(input)) {
                 input = scanner.nextLine();
             }
-        } else if (tag.equals("date")) {
+        } else if (tag.equalsIgnoreCase("date")) {
             while (!validator.validateDate(input)) {
                 input = scanner.nextLine();
             }
+        } else {
+            return null;
         }
         return input;
     }
 
-    public double getDouble(String tag) {
+    public Double getDouble(String tag) {
         String input = scanner.nextLine();
 
-        if (tag.equals("time")) {
+        if (tag.equalsIgnoreCase("time")) {
             while (!validator.validateTime(input)) {
                 input = scanner.nextLine();
             }
+        } else {
+            return null;
         }
         return Double.valueOf(input);
     }
 
-    public int getInt(String tag) {
+    public Integer getInt(String tag) {
         String input = scanner.nextLine();
 
-        if (tag.equals("tableNumber")) {
+        if (tag.equalsIgnoreCase("tablenumber")) {
             while (!validator.validateTablenumber(input)) {
                 input = scanner.nextLine();
             }
-        } else if (tag.equals("people")) {
+        } else if (tag.equalsIgnoreCase("people")) {
             while (!validator.validatePersons(input)) {
                 input = scanner.nextLine();
             }
-        } else if (tag.equals("selection")) {
+        } else if (tag.equalsIgnoreCase("selection")) {
             while (!validator.validateSelection(input)) {
                 input = scanner.nextLine();
             }
+        } else {
+            return null;
         }
         return Integer.valueOf(input);
     }
